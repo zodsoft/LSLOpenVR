5f8a4390a7b1389942c7606a413a1e83a444c207
Chadwick Boulay <chadwick.boulay@gmail.com>
2016-03-10 16:49:51 -0500

    It works! But the pose data for the controllers seems pretty strange. I'll have to visualize it to see.

--- src/SendOpenVR.cpp
+++ src/SendOpenVR.cpp
@@ -18,29 +18,51 @@ std::string GetTrackedDeviceString(vr::IVRSystem *pHmd, vr::TrackedDeviceIndex_t
 	return sResult;
 }
 
-
-void ProcessVREvent(const vr::VREvent_t & event)
+void
+PrintPose(vr::HmdMatrix34_t pose_mat)
 {
-	switch (event.eventType)
-	{
-	case vr::VREvent_TrackedDeviceActivated:
-	{
-		//SetupRenderModelForTrackedDevice(event.trackedDeviceIndex);
-		std::cout << "Attached device" << event.trackedDeviceIndex << std::endl;
-	}
-	break;
-	case vr::VREvent_TrackedDeviceDeactivated:
-	{
-		std::cout << "Detached device " << event.trackedDeviceIndex << std::endl;
-	}
-	break;
-	case vr::VREvent_TrackedDeviceUpdated:
-	{
-		std::cout << "Updated device " << event.trackedDeviceIndex << std::endl;
-	}
-	break;
-	}
-}
+	double clk = lsl::local_clock();
+	std::cout
+		<< fmod(1000.0*clk, 1000)
+		<< ", "
+		<< pose_mat.m[0][0]
+		<< ", "
+		<< pose_mat.m[0][1]
+		<< ", "
+		<< pose_mat.m[0][2]
+		<< ", "
+		<< pose_mat.m[0][3]
+		<< ", "
+		<< pose_mat.m[1][0]
+		<< ", "
+		<< pose_mat.m[1][1]
+		<< ", "
+		<< pose_mat.m[1][2]
+		<< ", "
+		<< pose_mat.m[1][3]
+		<< ", "
+		<< pose_mat.m[2][0]
+		<< ", "
+		<< pose_mat.m[2][1]
+		<< ", "
+		<< pose_mat.m[2][2]
+		<< ", "
+		<< pose_mat.m[2][3]
+		<< std::endl;
+};
+
+const int n_devices = 3;
+std::vector<std::string> device_chans = {
+	"HMD",
+	"Left",
+	"Right"
+};
+const int n_pose_axes = 3 * 4;
+std::vector<std::string> pose_chans = {
+	"00", "01", "02", "03",
+	"10", "11", "12", "13",
+	"20", "21", "22", "23"
+};
 
 
 int main(int argc, char* argv[]) {
@@ -52,10 +74,6 @@ int main(int argc, char* argv[]) {
 	//TODO: Parse pollControllers to a boolean
 
     try {
-
-        // TODO: Open libopenvr, get device parameters
-        // Need nChannels and maybe poll a few samples to get sampRate
-        // Device name
 		vr::EVRInitError eError = vr::VRInitError_None;
 		vr::IVRSystem *vrsys = vr::VR_Init(&eError, vr::VRApplication_Other);
 		if (eError != vr::VRInitError_None)
@@ -67,76 +85,156 @@ int main(int argc, char* argv[]) {
 			return false;
 		}
 
-		std::string strDriver = "No Driver";
-		std::string strDisplay = "No Display";
+		std::string manufac = GetTrackedDeviceString(vrsys, vr::k_unTrackedDeviceIndex_Hmd, vr::Prop_ManufacturerName_String);
+		std::cout
+			<< "Manufacturer: " << manufac
+			<< "; Driver: " << GetTrackedDeviceString(vrsys, vr::k_unTrackedDeviceIndex_Hmd, vr::Prop_TrackingSystemName_String)
+			<< "; Display: " << GetTrackedDeviceString(vrsys, vr::k_unTrackedDeviceIndex_Hmd, vr::Prop_SerialNumber_String)
+			<< std::endl;
+		//Manufacturer: HTC; Driver: lighthouse; Display: LHR-5D35FA67
 
-		strDriver = GetTrackedDeviceString(vrsys, vr::k_unTrackedDeviceIndex_Hmd, vr::Prop_TrackingSystemName_String);
-		strDisplay = GetTrackedDeviceString(vrsys, vr::k_unTrackedDeviceIndex_Hmd, vr::Prop_SerialNumber_String);
-		std::cout << "Driver: " << strDriver << "; Display: " << strDisplay << std::endl;
-		//Driver: lighthouse; Display: LHR-5D35FA67
+		if (vrsys->IsInputFocusCapturedByAnotherProcess())
+		{
+			std::cout << "Input focus is captured by another process" << std::endl;
+		}
 
-		bool deviceIsTracked[vr::k_unMaxTrackedDeviceCount];
+		// Wait until all devices are tracking
 
-		/*
-		vr::TrackingUniverseOrigin eOrigin = vr::TrackingUniverseSeated;
-		vr::TrackedDevicePose_t *pTrackedDevicePoseArray;
-		uint32_t unTrackedDevicePoseArrayCount = 1;
-		*/
-        
-        /*
-        // TODO: make 2 new stream_info. One for Button events, one for Position
-        lsl::stream_info infoButtons("OpenVRButtons", "Markers", 1, lsl::IRREGULAR_RATE, lsl::cf_string, "OpenVRButtons_");
-        //TODO: Add XML info
-        //TODO: Get manufacturer from libopenvr
-        //info.desc().append_child_value("manufacturer", "HTC Vive");
+		std::vector<uint32_t> device_indices = {
+			vr::k_unTrackedDeviceIndex_Hmd,
+			(uint32_t)vrsys->GetTrackedDeviceIndexForControllerRole(vr::TrackedControllerRole_RightHand),
+			(uint32_t)vrsys->GetTrackedDeviceIndexForControllerRole(vr::TrackedControllerRole_LeftHand)
+		};
+		std::cout << device_indices[0] << " " << device_indices[1] << " " << device_indices[2] << std::endl;
+		
+		//Make stream infos. First for Button events. Second for poses.
+        lsl::stream_info infoButtons("Buttons", "Markers", 1, lsl::IRREGULAR_RATE, lsl::cf_int8, "OpenVRButtons_");
+		infoButtons.desc().append_child_value("manufacturer", manufac);
+		lsl::xml_element butt_channels = infoButtons.desc().append_child("channels");
+		butt_channels.append_child("channel")
+			.append_child_value("label", "Controllers")
+			.append_child_value("type", "ButtonCode")
+			.append_child_value("unit", "uint8_code");
         lsl::stream_outlet outletButtons(infoButtons);
-        
-        lsl::stream_info infoPoses("OpenVRPoses", "Position", 36, 60, lsl::cf_float32, "OpenVRPoses_");
-        // TODO: Add XML info
-        //info.desc().append_child_value("manufacturer", "HTC Vive");
-        //lsl::xml_element channels = info.desc().append_child("channels");
-        //channels.append_child("channel")
-        //  .append_child_value("label","X")
-        //  .append_child_value("type","PositionX")
-        //  .append_child_value("unit","normalized_signed");
+        		
+        lsl::stream_info infoPoses("OpenVRPoses", "Position", n_devices*n_pose_axes, 300, lsl::cf_float32, "OpenVRPoses_");
+		infoPoses.desc().append_child_value("manufacturer", manufac);
+        lsl::xml_element pose_channels = infoPoses.desc().append_child("channels");
+		int device_id;  // Index into our list of 3 devices: HMD, Left, Right
+		int pose_ax;
+		for (device_id = 0; device_id < n_devices; device_id++)
+		{
+			for (pose_ax = 0; pose_ax < n_pose_axes; pose_ax++)
+			{
+				std::string ch_label = device_chans[device_id] + pose_chans[pose_ax];
+				pose_channels.append_child("channel")
+					.append_child_value("label", ch_label)
+					.append_child_value("type", "Pose")
+					.append_child_value("unit", "m");
+			}
+		}
         lsl::stream_outlet outletPoses(infoPoses);
-        */
         
         // send data forever
         std::cout << "Now sending data... " << std::endl;
-        
         for(unsigned t=0;;t++) {
 
-			// Process SteamVR events
+			// Process SteamVR events (i.e., button presses)
 			vr::VREvent_t event;
 			while (vrsys->PollNextEvent(&event, sizeof(event)))
 			{
-				ProcessVREvent(event);
-			}
-
-			// Process SteamVR controller state
-			for (vr::TrackedDeviceIndex_t unDevice = 0; unDevice < vr::k_unMaxTrackedDeviceCount; unDevice++)
-			{
-				vr::VRControllerState_t state;
-				if (vrsys->GetControllerState(unDevice, &state))
+				vr::TrackedDeviceIndex_t devId = event.trackedDeviceIndex;
+				vr::ETrackedDeviceClass devClass = vrsys->GetTrackedDeviceClass(devId);  // TrackedDeviceClass_Controller = 2
+				if (devClass == vr::TrackedDeviceClass_Controller)
 				{
-					deviceIsTracked[unDevice] = state.ulButtonPressed == 0;
+					// TrackedControllerRole_LeftHand or TrackedControllerRole_RightHand
+					vr::ETrackedControllerRole ctrlRole = vrsys->GetControllerRoleForTrackedDeviceIndex(devId);
+
+					//0 = k_EButton_System; 1 = k_EButton_ApplicationMenu; 2 = k_EButton_Grip (either); 32 = k_EButton_Axis0 = k_EButton_SteamVR_Touchpad; 33 = k_EButton_Axis1 = k_EButton_SteamVR_Trigger
+					vr::EVRButtonId butId = (vr::EVRButtonId)event.data.controller.button;
+
+					//VREvent_TrackedDeviceActivated = 100; VREvent_TrackedDeviceUserInteractionStarted = 103; VREvent_TrackedDeviceUserInteractionEnded = 104; VREvent_ButtonPress = 200; VREvent_ButtonUnpress = 201; VREvent_ButtonTouch = 202; VREvent_ButtonUntouch = 203;
+					vr::EVREventType evType = (vr::EVREventType)event.eventType;
+
+					// We are interested in 2 controllers. Use 1-2 MSB (markerVal >> 7)
+					// We are interested in 4 buttons. Use 3-5 MSB ((markerVal % 128) >> 3)
+					// We are interested in 7 events. Use 6-8 MSB (markerVal % 8)
+					uint8_t markerVal = 0;
+					if (ctrlRole == vr::TrackedControllerRole_LeftHand)
+					{
+						markerVal |= (1 << 7);
+					}
+					
+					uint32_t button = butId;
+					if (button >= 32)
+					{
+						button -= 29;
+					}
+					markerVal |= button << 3;
+
+					uint32_t event = evType;
+					event -= 100;
+					if (event >= 3)
+					{
+						event -= 2;
+					}
+					if (event >= 98)
+					{
+						event -= 95;
+					}
+					markerVal |= event;
+
+					const int sample = markerVal;
+					outletButtons.push_sample(&sample);
+					
+					std::cout
+						<< ((ctrlRole == vr::TrackedControllerRole_LeftHand) ? "Left" : "Right")
+						<< "; Button: " << vrsys->GetButtonIdNameFromEnum(butId)
+						<< "; Event: " << vrsys->GetEventTypeNameFromEnum(evType)
+						<< "; Marker Val: " << static_cast<int16_t>(markerVal)
+						<< std::endl;
 				}
 			}
-            
-            //TODO: Get button events from openvr
-            //TODO: If there were button events, then push a sample through outletButtons
-			//vrsys->GetDeviceToAbsoluteTrackingPose(eOrigin, 0.0f, VR_ARRAY_COUNT(unTrackedDevicePoseArrayCount) pTrackedDevicePoseArray, unTrackedDevicePoseArrayCount);
-            
-            //TODO: Get the state and pose from openvr
-            //Always push the latest sample
-            /*
-            float sample[36] = {js.lX,js.lY,js.lZ,js.lRx,js.lRy,js.lRz,js.rglSlider[0],js.rglSlider[1],
-                js.rgdwPOV[0],js.rgdwPOV[1],js.rgdwPOV[2],js.rgdwPOV[3],js.lVX,js.lVY,js.lVZ,js.lVRx,js.lVRy,js.lVRz,
-                js.rglVSlider[0],js.rglVSlider[1],js.lAX,js.lAY,js.lAZ,js.lARx,js.lARy,js.lARz,js.rglASlider[0],
-                js.rglASlider[1],js.lFX,js.lFY,js.lFZ,js.lFRx,js.lFRy,js.lFRz,js.rglFSlider[0],js.rglFSlider[1]};
-            outletPoses.push_sample(sample);
-            */
+
+			// Get the poses
+			vr::TrackedDevicePose_t rTrackedDevicePose[vr::k_unMaxTrackedDeviceCount];
+			float pose_sample[n_devices*n_pose_axes];
+			vrsys->GetDeviceToAbsoluteTrackingPose(vr::TrackingUniverseSeated, 0.0f, VR_ARRAY_COUNT(vr::k_unMaxTrackedDeviceCount) rTrackedDevicePose, vr::k_unMaxTrackedDeviceCount);
+			for (int device_ix = 0; device_ix < vr::k_unMaxTrackedDeviceCount; device_ix++)
+			{
+				if (rTrackedDevicePose[device_ix].bPoseIsValid)
+				{
+					vr::HmdMatrix34_t pose_mat = rTrackedDevicePose[device_ix].mDeviceToAbsoluteTracking;
+
+					if (device_ix == vr::k_unTrackedDeviceIndex_Hmd)
+					{
+						device_id = 0;
+					}
+					else if (device_ix == vrsys->GetTrackedDeviceIndexForControllerRole(vr::TrackedControllerRole_LeftHand))
+					{
+						device_id = 1;
+					}
+					else if (device_ix == vrsys->GetTrackedDeviceIndexForControllerRole(vr::TrackedControllerRole_RightHand))
+					{
+						device_id = 2;
+					}
+
+					if (device_id == 99)
+					{
+						PrintPose(pose_mat);
+					}
+					
+					for (int row_ix = 0; row_ix < 3; row_ix++)
+					{
+						for (int col_ix = 0; col_ix < 4; col_ix++)
+						{
+							int sample_ix = (3 * 4 * device_id) + (4 * row_ix) + col_ix;
+							pose_sample[sample_ix] = pose_mat.m[row_ix][col_ix];
+						}
+					}
+					outletPoses.push_sample(pose_sample);
+				}
+			}	
         }
 
     } catch(std::exception &e) {
